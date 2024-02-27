79e61c462378df7d86527442ca636adbdb00cef0
Anna Sumire <anachikhladze99@gmail.com>
2024-02-27 11:55:47 +0400

    feat: initial commit

---
+++ apple-gallery/apple_galleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_galleryApp.swift
+//  apple-gallery
+//
+//  Created by Anna Sumire on 27.02.24.
+//
+
+import SwiftUI
+
+@main
+struct apple_galleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
