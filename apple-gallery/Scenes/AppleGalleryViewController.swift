8375e0417177f52e1c39ec4a687ae7e21fd4c556
Anna Sumire <anachikhladze99@gmail.com>
2024-02-28 15:11:58 +0400

    feat: add tableView and set constraints

--- apple-gallery/Scenes/AppleGalleryViewController.swift
+++ apple-gallery/Scenes/AppleGalleryViewController.swift
@@ -8,9 +8,38 @@
 import UIKit
 
 final class AppleGalleryViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    // MARK: - ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupBackground()
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    // MARK: - Private Methods
+    private func setupBackground() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupSubviews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+        ])
     }
 }
